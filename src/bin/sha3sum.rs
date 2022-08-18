4d3c8039307498687f1116f308f296b3f420d91e
iczero <iczero4@gmail.com>
2022-08-18 00:50:06 -0700

    implement generic for SHA3 family

--- src/bin/sha3sum.rs
+++ src/bin/sha3sum.rs
@@ -1,9 +1,10 @@
 #![feature(slice_as_chunks)]
+use derp_keccak::hash::{Digest, KeccakHash, SHA3_256, SHA3_384, SHA3_512};
 use derp_keccak::util::bytes_to_hex;
 use derp_keccak::Keccak;
 use std::io::{BufReader, Read};
 
-fn main() -> anyhow::Result<()> {
+fn main_old() -> anyhow::Result<()> {
     let mut keccak = Keccak::new();
 
     let mut stdin = BufReader::new(std::io::stdin());
@@ -30,6 +31,50 @@ fn main() -> anyhow::Result<()> {
 
     let out = keccak.squeeze_many(bitrate, 256 / 8);
     println!("{}", bytes_to_hex(&out));
+    Ok(())
+}
+
+fn main_keccakhash<T: KeccakHash>(mut hash: T) -> anyhow::Result<()> {
+    let mut stdin = std::io::stdin();
+    const READ_SIZE: usize = 16384;
+    let mut buf: Vec<u8> = Vec::with_capacity(READ_SIZE);
+    buf.resize(READ_SIZE, 0);
+
+    loop {
+        let len = stdin.read(&mut buf)?;
+        if len == 0 {
+            break;
+        }
+        hash.update(&buf[0..len]);
+    }
+
+    println!("{}", hash.finalize().hex());
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.get(1) == None {
+        main_keccakhash(SHA3_256::new())?;
+    } else {
+        match args[1].as_str() {
+            "256" => {
+                main_keccakhash(SHA3_256::new())?;
+            },
+            "384" => {
+                main_keccakhash(SHA3_384::new())?;
+            },
+            "512" => {
+                main_keccakhash(SHA3_512::new())?;
+            },
+            "old" => {
+                main_old()?;
+            },
+            _ => {
+                eprintln!("invalid hash length");
+            }
+        }
+    }
     Ok(())
 }
 
---
+++ src/hash.rs
@@ -0,0 +1,144 @@
+use crate::util::bytes_to_hex;
+use crate::Keccak;
+
+pub struct DigestBytes<const D: usize> {
+    pub data: [u8; D],
+}
+
+pub trait Digest {
+    fn hex(&self) -> String;
+}
+
+impl<const D: usize> Digest for DigestBytes<D> {
+    fn hex(&self) -> String {
+        bytes_to_hex(&self.data)
+    }
+}
+
+// type parameters:
+// CAPACITY: capacity (512 bits for sha3-256)
+// BUFFER_LEN: buffer length (must be equivalent to byte length of bitrate)
+// DIGEST_BYTES: digest length in bytes
+// PAD_BITS: extra bits in padding
+// PAD_BITS_LEN: how many bits in PAD_BITS
+pub struct KeccakHashState<
+    const CAPACITY: usize,
+    const BUFFER_LEN: usize,
+    const DIGEST_BYTES: usize,
+    const PAD_BITS: u8,
+    const PAD_BITS_LEN: u8,
+> {
+    pub state: Keccak,
+    pub buffer: [u8; BUFFER_LEN],
+    pub buffer_pos: usize,
+    _private_construct: (),
+}
+
+pub trait KeccakHash {
+    type DigestType: Digest;
+
+    const CAPACITY: usize;
+    const RATE: usize;
+    const CAPACITY_BYTES: usize;
+    const RATE_BYTES: usize;
+
+    fn update(&mut self, data: &[u8]);
+    fn finalize(self) -> Self::DigestType;
+}
+
+impl<
+        const C: usize,
+        const BUFFER_LEN: usize,
+        const DIGEST_BYTES: usize,
+        const PAD_BITS: u8,
+        const PAD_BITS_LEN: u8,
+    > KeccakHashState<C, BUFFER_LEN, DIGEST_BYTES, PAD_BITS, PAD_BITS_LEN>
+{
+    pub fn new() -> KeccakHashState<C, BUFFER_LEN, DIGEST_BYTES, PAD_BITS, PAD_BITS_LEN> {
+        assert!(C % 64 == 0, "capacity must be a multiple of 64 bits");
+        assert!(
+            (1600 - C) / 8 == BUFFER_LEN,
+            "provided capacity does not match buffer length"
+        );
+        assert!(DIGEST_BYTES * 8 <= 1600, "digest length too long");
+        KeccakHashState {
+            state: Keccak::new(),
+            buffer: [0u8; BUFFER_LEN],
+            buffer_pos: 0,
+            _private_construct: (),
+        }
+    }
+}
+
+impl<
+        const C: usize,
+        const BUFFER_LEN: usize,
+        const DIGEST_BYTES: usize,
+        const PAD_BITS: u8,
+        const PAD_BITS_LEN: u8,
+    > KeccakHash for KeccakHashState<C, BUFFER_LEN, DIGEST_BYTES, PAD_BITS, PAD_BITS_LEN>
+{
+    type DigestType = DigestBytes<DIGEST_BYTES>;
+
+    const CAPACITY: usize = C;
+    const RATE: usize = 1600 - Self::CAPACITY;
+    const CAPACITY_BYTES: usize = Self::CAPACITY / 8;
+    const RATE_BYTES: usize = Self::RATE / 8;
+
+    fn update(&mut self, data: &[u8]) {
+        let mut index: usize = 0;
+        if self.buffer_pos + data.len() >= self.buffer.len() {
+            if self.buffer_pos > 0 {
+                index += self.buffer.len() - self.buffer_pos;
+                // data exists in buffer, fill remainder
+                self.buffer[self.buffer_pos..].copy_from_slice(&data[0..index]);
+                // absorb buffer
+                unsafe {
+                    self.state.absorb_block_unchecked(&self.buffer);
+                }
+                self.buffer_pos = 0;
+            }
+        } else {
+            // data fits entirely in current buffer
+            self.buffer[self.buffer_pos..self.buffer_pos + data.len()].copy_from_slice(&data);
+            self.buffer_pos += data.len();
+            return;
+        }
+
+        // no buffered data exists, absorb full blocks from data
+        while index + Self::RATE_BYTES <= data.len() {
+            unsafe {
+                self.state
+                    .absorb_block_unchecked(&data[index..index + Self::RATE_BYTES]);
+            }
+            index += Self::RATE_BYTES;
+        }
+
+        // if excess data exists, push to buffer
+        if data.len() - index > 0 {
+            let remaining = &data[index..];
+            self.buffer[0..remaining.len()].copy_from_slice(remaining);
+            self.buffer_pos = remaining.len();
+        }
+    }
+
+    fn finalize(mut self) -> DigestBytes<DIGEST_BYTES> {
+        // write buffer to keccak (or padding block if buffer is empty)
+        self.state.absorb_padded(
+            Self::RATE,
+            &self.buffer[0..self.buffer_pos],
+            PAD_BITS,
+            PAD_BITS_LEN,
+        );
+        let digest_bytes = self.state.squeeze_many(Self::RATE, DIGEST_BYTES);
+        let mut digest = DigestBytes {
+            data: [0u8; DIGEST_BYTES],
+        };
+        digest.data.copy_from_slice(&digest_bytes);
+        digest
+    }
+}
+
+pub type SHA3_256 = KeccakHashState<512, 136, 32, 0b10, 2>;
+pub type SHA3_384 = KeccakHashState<768, 104, 48, 0b10, 2>;
+pub type SHA3_512 = KeccakHashState<1024, 72, 64, 0b10, 2>;
